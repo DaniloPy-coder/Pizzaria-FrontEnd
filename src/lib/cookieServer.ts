59a7b85ffe38a0cfae169932e4f35267f1f3edef
DaniloPy-coder <danilojosehapper@gmail.com>
2025-04-14 19:41:02 -0300

    Primeiro commit do projeto Pizzaria

--- src/lib/cookieServer.ts
+++ src/lib/cookieServer.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
-export async function getCookieServer() {
-  const cookieStore = await cookies();
+export function getCookieServer() {
+  const cookieStore = cookies();
   const token = cookieStore.get("session")?.value;
 
   return token || null;
